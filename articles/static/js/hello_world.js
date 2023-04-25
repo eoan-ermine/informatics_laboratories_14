37265fdf1bea866ad923d1767d93267434a6fd07
PatriotRossii <patriotrossii2019@mail.ru>
2023-04-25 22:32:36 +0300

    Simplify fold handler

--- articles/static/js/hello_world.js
+++ articles/static/js/hello_world.js
@@ -63,31 +63,13 @@ var filterStudentsMarks = function(students) {
 var foldBtns = document.getElementsByClassName("fold-button");
 for (var i = 0; i != foldBtns.length; i++) {
 	foldBtns[i].addEventListener("click", function(event) {
-		if (event.target.className == "fold-button folded") {
-			event.target.innerHTML = "свернуть";
-			event.target.className = "fold-button";
-			var displayState = "block";
+		var parent = event.target.parentElement.parentElement;
+		if (parent.className == "one-post") {
+			event.target.innerHTML = "Развернуть";
+			parent.className = "one-post folded"
 		} else {
-			event.target.innerHTML = "развернуть";
-			event.target.className = "fold-button folded";
-			var displayState = "none";
+			event.target.innerHTML = "Свернуть";
+			parent.className = "one-post";
 		}
-
-		event.target
-			.parentElement
-			.parentElement
-			.getElementsByClassName("article-author")[0]
-			.style.display = displayState;
-		event.target
-			.parentElement
-			.parentElement
-			.getElementsByClassName("article-created-date")[0]
-			.style.display = displayState;
-		event.target
-			.parentElement
-			.parentElement
-			.getElementsByClassName("article-text")[0]
-			.style.display = displayState;
-		event.target.innerHTML = "развернуть";
 	});
 }
