bfbf77d7d97e6c875cb2cc654a9d213d4f8cdae6
PatriotRossii <patriotrossii2019@mail.ru>
2023-04-25 22:23:49 +0300

    Implement fold button

--- articles/static/js/hello_world.js
+++ articles/static/js/hello_world.js
@@ -59,3 +59,35 @@ var filterStudentsMarks = function(students) {
 		) / student["marks"].length) >= average
 	);
 };
+
+var foldBtns = document.getElementsByClassName("fold-button");
+for (var i = 0; i != foldBtns.length; i++) {
+	foldBtns[i].addEventListener("click", function(event) {
+		if (event.target.className == "fold-button folded") {
+			event.target.innerHTML = "свернуть";
+			event.target.className = "fold-button";
+			var displayState = "block";
+		} else {
+			event.target.innerHTML = "развернуть";
+			event.target.className = "fold-button folded";
+			var displayState = "none";
+		}
+
+		event.target
+			.parentElement
+			.parentElement
+			.getElementsByClassName("article-author")[0]
+			.style.display = displayState;
+		event.target
+			.parentElement
+			.parentElement
+			.getElementsByClassName("article-created-date")[0]
+			.style.display = displayState;
+		event.target
+			.parentElement
+			.parentElement
+			.getElementsByClassName("article-text")[0]
+			.style.display = displayState;
+		event.target.innerHTML = "развернуть";
+	});
+}
